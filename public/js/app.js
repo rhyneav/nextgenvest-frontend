66c6b36444af236739618104d8dfa7d1e2e53dbc
rhyneav <rhyneav@gmail.com>
2017-04-01 12:28:19 -0400

    Add loanService service to similate call to backend and loanOptions controller to display the data

--- public/js/app.js
+++ public/js/app.js
@@ -4,8 +4,50 @@ app.controller('infoCtrl', function($scope) {
     $scope.isWorking= "Yes";
 });
 
+app.controller('loanOptions', function($scope, loanService) {
+    $scope.loanOptions = [];
+
+    $scope.getLoanOptions = function() {
+        loanService.getLoanOptions().then(function (res) {
+            $scope.loanOptions = res;
+        });
+    };
+    $scope.getLoanOptions();
+});
+
 app.controller('loanForm', function($scope) {
     $scope.submit = function(loan) {
         console.log(loan);
+    }
+});
+
+app.service('loanService', function($timeout) {
+    var loanOptions =  [
+        {
+            name: 'Direct Subsidized Loan',
+            interest: 1
+        },
+        {
+            name: 'Direct Unsubsidized Loan',
+            interest: 2
+        },
+        {
+            name: 'A private bank loan',
+            interest: 3
+        },
+        {
+            name: 'A loan from the university',
+            interest: 4
+        },
+    ];
+
+    var getLoanOptions = function() {
+        return $timeout(function() {
+            return loanOptions
+        }, 1000);
+    };
+
+    return {
+        getLoanOptions: getLoanOptions
     }
 });
