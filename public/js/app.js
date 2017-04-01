b4751eed069ba7852aeba8fa4ede9cb466f8b694
rhyneav <rhyneav@gmail.com>
2017-04-01 12:06:56 -0400

    Create form and log submited fields to console

--- public/js/app.js
+++ public/js/app.js
@@ -3,3 +3,9 @@ var app = angular.module('loanApp', []);
 app.controller('infoCtrl', function($scope) {
     $scope.isWorking= "Yes";
 });
+
+app.controller('loanForm', function($scope) {
+    $scope.submit = function(loan) {
+        console.log(loan);
+    }
+});
